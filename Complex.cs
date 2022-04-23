177abde24f53743e06f451233b358fa112415edb
Your Name <you@example.com>
2022-04-23 19:25:47 +0300

    first commit

---
+++ Complex.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba4_Task
+{
+    class Complex
+    {
+        //i - imaginary unit
+        //a-Re
+        //b-Im
+
+        double re;
+        double im;
+
+        public Complex(double re, double im)
+        {
+            Re = re;
+            Im = im;
+        }
+
+        public double Re { get => re; set => re = value; }
+        public double Im { get => im; set => im = value; }
+
+        public static Complex operator +(Complex oneNumber, Complex twoNumber)
+        {
+            return new Complex(oneNumber.Re + twoNumber.Re, oneNumber.Im + twoNumber.Im);
+        }
+
+        public static Complex operator +(Complex oneNumber, double number)
+        {
+            return new Complex(oneNumber.Re + number, oneNumber.Im );
+        }
+
+        public static Complex operator +(double number, Complex oneNumber)
+        {
+            return new Complex(oneNumber.Re + number, oneNumber.Im);
+        }
+
+        public static Complex operator -(Complex oneNumber, Complex twoNumber)
+        {
+            return new Complex(oneNumber.Re - twoNumber.Re, oneNumber.Im - twoNumber.Im);
+        }
+
+        public static Complex operator /(Complex oneNumber, Complex twoNumber)
+        {
+            double z = twoNumber.Re * twoNumber.Re + twoNumber.Im * twoNumber.Im;
+            return new Complex((oneNumber.Re * twoNumber.Re + oneNumber.Im * twoNumber.Im) / z, (twoNumber.Re * oneNumber.Im - oneNumber.Re * twoNumber.Im) / z);
+
+        }
+
+        public static Complex operator *(Complex oneNumber, Complex twoNumber)
+        {
+            return new Complex(oneNumber.Re * twoNumber.Re - oneNumber.Im * twoNumber.Im, oneNumber.Re * twoNumber.Im + oneNumber.Im * twoNumber.Re);
+        }
+
+        public static Complex operator ++(Complex oneNumber)
+        {
+            return new Complex(oneNumber.Re++, oneNumber.Im++);
+        }
+
+        public static Complex operator --(Complex oneNumber)
+        {
+            return new Complex(oneNumber.Re--, oneNumber.Im--);
+        }
+
+        public static bool operator ==(Complex oneNumber, Complex twoNumber)
+        {
+            return oneNumber.Re == twoNumber.Re && oneNumber.Im == twoNumber.Im;
+        }
+
+        public static bool operator !=(Complex oneNumber, Complex twoNumber)
+        {
+            return oneNumber.Re != twoNumber.Re || oneNumber.Im != twoNumber.Im;
+        }
+
+
+        public double module()
+        {
+            return Math.Sqrt(Re * Re + Im * Im);
+        }
+
+        public static bool operator <(Complex oneNumber, Complex twoNumber)
+        {
+            return oneNumber.module() < twoNumber.module();
+        }
+
+        public static bool operator >(Complex oneNumber, Complex twoNumber)
+        {
+            return oneNumber.module() > twoNumber.module();
+        }
+
+        public static bool operator true(Complex oneNumber)
+        {
+            return oneNumber.Re != 0 || oneNumber.Im != 0;
+        }
+
+        public static bool operator false(Complex oneNumber)
+        {
+            return oneNumber.Re == 0 && oneNumber.Im == 0;
+        }
+        public double this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                {
+                    return Re;
+                }
+                if (index == 1)
+                {
+                    return Im;
+                }
+                throw new Exception("Invalid index");
+            }
+            set
+            {
+                if (index == 0)
+                {
+                    Re = value;
+                }
+                if (index == 1)
+                {
+                    Im = value;
+                }
+            }
+        }
+
+        public static implicit operator Complex(double number)
+        {
+            return new Complex(number, 0);
+        }
+
+        public static explicit operator double(Complex oneNumber)
+        {
+            return oneNumber.module();
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Re:0.##} + ({Im:0.##}) * i";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Laba4_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex firstComplexNumber = new Complex(5.5, -10.8); 
+            Console.WriteLine($"FirstComplexNumber = {firstComplexNumber}");
+            Complex secondComplexNumber = new Complex(7.4, -4.5);
+            Console.WriteLine($"SecondComplexNumber = {secondComplexNumber}");
+
+            Console.WriteLine("----------------------------------------------------");
+            Complex sumComplexNumber = firstComplexNumber + secondComplexNumber;
+            Console.WriteLine($"SumComplexNumber = {sumComplexNumber}");
+            Complex subtractionComplexNumber = firstComplexNumber - secondComplexNumber;
+            Console.WriteLine($"SubtractionComplexNumber = {subtractionComplexNumber}");
+
+            Console.WriteLine("----------------------------------------------------");
+            double realNumberOne = 12.5;
+            Console.WriteLine($"RealNumberOne = {realNumberOne}");
+            Complex sumComplexAndRealNumber = sumComplexNumber + realNumberOne;
+            Console.WriteLine($"SumComplexAndRealNumber = {sumComplexAndRealNumber}");
+
+            Console.WriteLine("----------------------------------------------------");
+            double realNumberTwo = 9.8;
+            Console.WriteLine($"RealNumberTwo = {realNumberTwo}");
+            Complex sumRealAndComplexNumber = realNumberTwo + sumComplexNumber;
+            Console.WriteLine($"SumRealAndComplexNumber = {sumRealAndComplexNumber}");
+
+            Console.WriteLine("----------------------------------------------------");
+            Complex divisionComplexNumber = firstComplexNumber / secondComplexNumber;
+            Console.WriteLine($"DivisionComplexNumber = {divisionComplexNumber}");
+            Complex multiplicationComplexNumber = firstComplexNumber * secondComplexNumber;
+            Console.WriteLine($"MultiplicationComplexNumber = {multiplicationComplexNumber}");
+
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine($"FirstComplexNumber: {firstComplexNumber}");
+            Console.WriteLine($"FirstComplexNumber ++: {firstComplexNumber++}");
+            Console.WriteLine($"FirstComplexNumber --: {firstComplexNumber--}");
+
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine($"SecondComplexNumber != FirstComplexNumber: {secondComplexNumber != firstComplexNumber}");
+            Complex thirdComplexNumber = firstComplexNumber;
+            Console.WriteLine($"FirstComplexNumber == ThirdComplexNumber: {firstComplexNumber == thirdComplexNumber}");
+
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine($"firstComplexNumber < secondComplexNumber: {firstComplexNumber < secondComplexNumber}");
+            Console.WriteLine($"firstComplexNumber > secondComplexNumber: {firstComplexNumber > secondComplexNumber}");
+
+            Console.WriteLine("----------------------------------------------------");
+            Complex fouthComplexNumber = new Complex(0, 0);
+            Console.WriteLine($"fouthComplexNumber =  {fouthComplexNumber}" );
+            if (fouthComplexNumber)
+            {
+                Console.WriteLine("fouthComplexNumber = true");
+            }
+            else
+            {
+                Console.WriteLine("fouthComplexNumber = false");
+            }
+            Console.WriteLine("----------------------------------------------------");
+            Complex fifthComplexNumber = new Complex(0,0);
+            Console.WriteLine($"fifthComplexNumber = {fifthComplexNumber}");
+            fifthComplexNumber[0] = 16;
+            fifthComplexNumber[1] = -8;
+            fifthComplexNumber[2] = 100;
+            Console.WriteLine($"fifthComplexNumber = {fifthComplexNumber}");
+            try
+            {
+                Console.WriteLine($"fifthComplexNumber[0] = {fifthComplexNumber[0]}");
+                Console.WriteLine($"fifthComplexNumber[1] = {fifthComplexNumber[1]}");
+                Console.WriteLine($"fifthComplexNumber[2] = {fifthComplexNumber[2]}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Exception = {e.Message}");
+            }
+            Console.WriteLine("----------------------------------------------------");
+            double oneNumber = -17.5;
+            Console.WriteLine($"oneNumber = {oneNumber}");
+            Complex sixthComplexNumber = oneNumber; //неявно
+            Console.WriteLine($"sixthComplexNumber = {sixthComplexNumber}");
+            oneNumber = (double)thirdComplexNumber; //явно
+            Console.WriteLine($"OneNumber = {oneNumber}");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine($"sumComplexNumber = {sumComplexNumber}");
+            Console.WriteLine($"realNumberOne = {realNumberOne}");
+            Console.WriteLine($"sumComplexNumber / realNumberOne = {sumComplexNumber / realNumberOne}");
+
+
+        }
+    }
+}
